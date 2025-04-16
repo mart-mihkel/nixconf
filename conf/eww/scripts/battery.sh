88e28f17709d8eb2a59b9cd2d27d65825b0de661
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-04-16 15:39:33 +0300

    battery notifications🔋

--- conf/eww/scripts/battery.sh
+++ conf/eww/scripts/battery.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
-status=$(cat /sys/class/power_supply/BAT0/status)
-capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-idx=$(( capacity * 9 / 100 ))
+notify_status=3 # 3: > 20 | charging, 2: <= 20, 1: <= 10
+bat="/sys/class/power_supply/BAT0"
 
-if [[ "$status" == "Charging" ]]; then
-    icons=("󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅")
-else
-    icons=("󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
-fi
+while true; do
+    status=$(cat "$bat/status")
+    capacity=$(cat "$bat/capacity")
+    idx=$(( capacity * 9 / 100 ))
 
-echo "${icons[$idx]}"
+    if [[ "$status" == "Charging" ]]; then
+        icons=("󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅")
+        notify_status=3
+    else
+        icons=("󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
+        if [[ $notify_status -gt 2 && $capacity -le 20 ]]; then
+            dunstify -u normal "󰂃 Low"
+            notify_status=2
+        elif [[ $notify_status -gt 1 && $capacity -le 10 ]]; then
+            dunstify -u critical "󰂃 Critical"
+            notify_status=1
+        fi
+    fi
+
+    echo "${icons[$idx]}"
+    sleep 2
+done
