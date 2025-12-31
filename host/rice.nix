5d6898acf208d973077704c9dd0a3e0be17b2e9a
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-12-31 13:58:11 +0200

    hyprland

---
+++ host/rice.nix
@@ -0,0 +1,43 @@
+# https://github.com/mart-mihkel/moomin.git
+{pkgs, ...}: {
+  programs.hyprland.enable = true;
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    material-symbols
+  ];
+
+  environment.systemPackages = with pkgs; [
+    wayland-pipewire-idle-inhibit
+    swaynotificationcenter
+    adwaita-icon-theme
+    brightnessctl
+    wl-clipboard
+    imagemagick
+    pulsemixer
+    wl-mirror
+    playerctl
+    gammastep
+    fastfetch
+    grimblast
+    alacritty
+    hyprland
+    hypridle
+    hyprlock
+    ghostty
+    hellwal
+    bluetui
+    cmatrix
+    ffmpeg
+    waybar
+    impala
+    wtype
+    dunst
+    rofi
+    swww
+    cava
+    tofi
+    foot
+    feh
+  ];
+}
